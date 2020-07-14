7d016688473bd6a828d446a7b55311d04e67a7b8
Onkar Bendre <36733796+Onkarbendre2000@users.noreply.github.com>
2020-07-15 00:36:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,5 +5,5 @@ var app = express();
 app.set('view engine','ejs');
 app.use('/assets',express.static('assets'))
 app.use('/controllers',express.static('controllers'))
-app.listen(8002,'0.0.0.0');
+app.listen(8080,'0.0.0.0');
 controller(app);
